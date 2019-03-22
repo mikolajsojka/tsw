e803795c063e86d6ce7bbd6c9c60c136dea88aa3
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-03-22 10:28:09 +0100

    lab5 zad1 mastermind

---
+++ lab05-21.03/zad1.js
@@ -0,0 +1,48 @@
+/* jshint strict: global, esversion: 6, devel: true */
+"use strict";
+
+const ocena = kod => {
+  return ruch => {
+    let visited = [];
+
+    let ruch_copy = ruch;
+
+    let result = {
+      white: 0,
+      black: 0
+    };
+
+    if (kod.length === ruch.length) {
+      ruch_copy.forEach((element, index) => {
+        if (element === kod[index] && !visited.includes(index)) {
+          visited.push(index);
+          delete ruch_copy[index];
+          result.black += 1;
+        }
+      });
+
+      kod.forEach((Kelement, Kindex) => {
+        ruch_copy.forEach((Relement, Rindex) => {
+          if (!visited.includes(Kindex) && Kelement === Relement) {
+            delete ruch_copy[Rindex];
+            visited.push(Kindex);
+            result.white += 1;
+          }
+        });
+      });
+
+      return `Czarne: ${result.black}, Białe: ${result.white}`;
+    } else {
+      throw { typerr: "Różne rozmiary tablic!" };
+    }
+  };
+};
+
+let kod = [7, 0, 0, 0];
+let ruch = [0, 7, 0, 0];
+
+try {
+  console.log(ocena(kod)(ruch));
+} catch (e) {
+  console.log(e.typerr);
+}
