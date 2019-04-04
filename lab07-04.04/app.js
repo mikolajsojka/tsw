0d90ea53510c0fc6404a80e105dc084a776d1b13
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-04-04 22:29:08 +0200

    C:/Program Files/Git/game/move

--- lab07-04.04/app.js
+++ lab07-04.04/app.js
@@ -6,17 +6,57 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
-const uuidv1 = require('uuid/v1');
+const uuidv1 = require("uuid/v1");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const ocena = kod => {
+  return ruch => {
+    let visited = [];
+
+    let ruch_copy = ruch;
+
+    let result = {
+      white: 0,
+      black: 0
+    };
+
+    if (kod.length === ruch.length) {
+      ruch_copy.forEach((element, index) => {
+        if (element === kod[index] && !visited.includes(index)) {
+          visited.push(index);
+          delete ruch_copy[index];
+          result.black += 1;
+        }
+      });
+
+      kod.forEach((Kelement, Kindex) => {
+        ruch_copy.forEach((Relement, Rindex) => {
+          if (!visited.includes(Kindex) && Kelement === Relement) {
+            delete ruch_copy[Rindex];
+            visited.push(Kindex);
+            result.white += 1;
+          }
+        });
+      });
+
+      ruch_copy = ruch_copy.filter(function(el) {
+        return el != undefined;
+      });
+
+      return result;
+    } else {
+      throw { typerr: "Różne rozmiary tablic!" };
+    }
+  };
+};
 
 app.get("/", (_req, res) => {
   res.send("Serwer działa pomyślnie");
 });
 
 app.post("/game/new", (req, res) => {
-  
   let newGameRes = {
     game: uuidv1(),
     size: req.body.size || 5,
@@ -25,6 +65,18 @@ app.post("/game/new", (req, res) => {
   };
 
   res.send(newGameRes);
+});
+
+app.post("/game/move", (req, res) => {
+  let code = [1, 2, 1, 2, 1];
+  let move = req.body.move;
+
+  try {
+    let result = ocena(code)(move);
+    res.send(result);
+  } catch (e) {
+    res.send(e.typerr);
+  }
 });
 
 app.listen(port, () => {
