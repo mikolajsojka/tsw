6a7dbbffddcb066bf91c9b69128a5d870918895d
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-04-15 22:39:24 +0200

    k

--- lab07-04.04/client/skrypt.js
+++ lab07-04.04/client/skrypt.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   var sendRequest, handleResponse;
 
   sendRequest = () => {
+    //localStorage.clear();
 
-    console.log(window.localStorage.getItem('game'));
     var xhr = new XMLHttpRequest();
     xhr.onload = () => {
       handleResponse(xhr);
@@ -19,8 +19,19 @@ document.addEventListener("DOMContentLoaded", () => {
   handleResponse = xhr => {
     if (xhr.status === 200) {
       let xmlRes = xhr.responseXML;
-      let test = xmlRes.getElementsByTagName('id')[0].firstChild.nodeValue;
-      window.localStorage.setItem('game', test);
+      let test = xmlRes.getElementsByTagName("id")[0].firstChild.nodeValue;
+
+      let element = {id:test};
+      let elements = [element];
+
+      let games = JSON.parse(window.localStorage.getItem("games"));
+
+      if(games){
+        games.push(element);
+        window.localStorage.setItem("games", JSON.stringify(games));
+      } else{
+        window.localStorage.setItem("games", JSON.stringify(elements));
+      }
 
       document.getElementById("new").style.display = "none";
       document.getElementById("gameId").style.display = "flex";
