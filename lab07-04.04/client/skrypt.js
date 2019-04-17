40887b295958835704931da6de36e9e2acc87fea
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-04-17 14:23:36 +0200

    code better

--- lab07-04.04/client/skrypt.js
+++ lab07-04.04/client/skrypt.js
@@ -3,51 +3,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   var sendRequest, handleResponse;
 
-  let LocalGames = JSON.parse(window.localStorage.getItem("games"));
-  let yourGames = document.getElementById("yourGames");
-  let actual = "";
-
-  if (LocalGames) {
-    LocalGames.forEach(element => {
-      actual += `</br><div id=${element.id} class="actualGame">${
-        element.id
-      }</div></br>`;
-    });
-  }
-
-  yourGames.innerHTML = actual;
-
-  sendRequest = () => {
-    //localStorage.clear();
-
+  sendRequest = (url, method, body) => {
     var xhr = new XMLHttpRequest();
     xhr.onload = () => {
       handleResponse(xhr);
     };
 
-    xhr.open("POST", "http://localhost:3000/game/new", true);
+    xhr.open(method, `http://localhost:3000/${url}`, true);
 
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({ size: 12 }));
+    xhr.send(body);
   };
 
   handleResponse = xhr => {
     if (xhr.status === 200) {
-      let xmlRes = xhr.responseXML;
-
-      let element = {
-        id: xmlRes.getElementsByTagName("id")[0].firstChild.nodeValue,
-        size: xmlRes.getElementsByTagName("size")[0].firstChild.nodeValue,
-        colors: xmlRes.getElementsByTagName("colors")[0].firstChild.nodeValue,
-        steps: xmlRes.getElementsByTagName("steps")[0].firstChild.nodeValue
-      };
-
-      let elements = [element];
-
-      let games = JSON.parse(window.localStorage.getItem("games"));
-
-      fillLocalStorageGames(elements, games, element);
+      handleNewGame(xhr);
     }
+  };
+
+  handleNewGame = xhr => {
+    let xmlRes = xhr.responseXML;
+
+    let element = {
+      id: xmlRes.getElementsByTagName("id")[0].firstChild.nodeValue,
+      size: xmlRes.getElementsByTagName("size")[0].firstChild.nodeValue,
+      colors: xmlRes.getElementsByTagName("colors")[0].firstChild.nodeValue,
+      steps: xmlRes.getElementsByTagName("steps")[0].firstChild.nodeValue
+    };
+
+    let elements = [element];
+
+    let games = JSON.parse(window.localStorage.getItem("games"));
+
+    fillLocalStorageGames(elements, games, element);
   };
 
   fillLocalStorageGames = (elements, games, element) => {
@@ -57,23 +45,47 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       window.localStorage.setItem("games", JSON.stringify(elements));
     }
-
     window.localStorage.setItem("currentGame", JSON.stringify(element));
-
     document.getElementById("new").style.display = "none";
-
     document.getElementById("gameId").style.display = "flex";
     document.getElementById("gameId").innerHTML = element.id;
   };
 
   actualGamesClick = () => {
+    let LocalGames = JSON.parse(window.localStorage.getItem("games"));
+    let yourGames = document.getElementById("yourGames");
+    let actual = "";
+
+    if (LocalGames) {
+      LocalGames.forEach(element => {
+        actual += `</br><div id=${element.id} class="actualGame">${
+          element.id
+        }</div></br>`;
+      });
+    }
+
+    yourGames.innerHTML = actual;
+
     document.getElementById("new").style.display = "none";
     document.getElementById("yourGames").style.display = "block";
   };
 
+  newGameClick = () => {
+    let body = JSON.stringify({ size: 12 });
+
+    sendRequest("game/new", "POST", body);
+  };
+
+  localStorageClick = () => {
+    localStorage.clear();
+  };
+
   let newGame = document.getElementById("newGame");
-  newGame.addEventListener("click", sendRequest, false);
+  newGame.addEventListener("click", newGameClick, false);
 
   let actualGames = document.getElementById("actualGames");
   actualGames.addEventListener("click", actualGamesClick, false);
+
+  let clearLocalStorage = document.getElementById("clearLocalStorage");
+  clearLocalStorage.addEventListener("click", localStorageClick, false);
 });
