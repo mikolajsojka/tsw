25812612c62cb6d63b47cb4cb8bfe2f90f124ce4
Mikołaj Sojka <msojka@sigma.ug.edu.pl>
2019-05-29 16:30:54 +0200

    backup

--- project-horses/client/src/views/Horse.vue
+++ project-horses/client/src/views/Horse.vue
@@ -14,8 +14,35 @@
         name: "Horse",
         data () {
             return {
-                deletecheck: 0
+                deletecheck: 0,
+                horse: {},
+                classes: "",
+                actualclass: ""
             };
+        },
+        created () {
+            Array.from(this.$store.state.horses).forEach(element => {
+                if (element._id === this.$route.params.id) {
+                    this.horse = element;
+
+                    this.classes = "<select>";
+                    Array.from(this.$store.state.classes).forEach(item => {
+                        if (element.class === item.number) {
+                            this.actualclass = item.category;
+                            this.classes += `<option selected="selected" value="element.class">${
+                                item.category
+                            }</option>`;
+                        } else {
+                            this.classes += `<option value="item.number">${
+                                item.category
+                            }</option>`;
+                        }
+                    });
+                    this.classes += "</select>";
+                }
+            });
+
+            console.log(this.horse);
         },
         methods: {
             deletehorse () {
@@ -25,87 +52,63 @@
                 }
                 this.deletecheck = 1;
             },
-            renderhorse () {
-                let horse;
-                let classes;
-                let actualclass;
-                Array.from(this.$store.state.horses).forEach(element => {
-                    if (element._id === this.$route.params.id) {
-                        horse = element;
 
-                        classes = "<select>";
-                        Array.from(this.$store.state.classes).forEach(item => {
-                            if (element.class === item.number) {
-                                actualclass = item.category;
-                                console.log(item.category);
-                                console.log(actualclass);
-                                classes += `<option selected="selected" value="element.class">${
-                                    item.category
-                                }</option>`;
-                            } else {
-                                classes += `<option value="item.number">${
-                                    item.category
-                                }</option>`;
-                            }
-                        });
-                        classes += "</select>";
-                    }
-                });
-                if (horse) {
+            renderhorse () {
+                if (this.horse) {
                     return `
                     <div id="info">
                         <div id="first">
                         <label>Imię</label>
-                        <input name="name" value="${horse.name}" />
+                        <input name="name" value="${this.horse.name}" />
                         <label>Numer startowy</label>
-                        <input name="number" value="${horse.number}"/>
+                        <input name="number" value="${this.horse.number}"/>
                         <label>Kraj Pochodzenia</label>
-                        <input name="country" value="${horse.country}"/>
+                        <input name="country" value="${this.horse.country}"/>
                         <label>Data urodzenia</label>
-                        <input name="yob" value="${horse.yob}"/>
+                        <input name="yob" value="${this.horse.yob}"/>
                         <label>Sierść</label>
-                        <input name="hair" value="${horse.hair}"/>
+                        <input name="hair" value="${this.horse.hair}"/>
                         <label>Płeć</label>
-                        <input name="sex" value="${horse.sex}"/>
+                        <input name="sex" value="${this.horse.sex}"/>
                         <label>Klasa startowa</label>
-                        ${classes}
+                        ${this.classes}
                         </div>  
 
                         <div id="second">
                         <label>Rodowód - Ojciec</label>
                         <input name="bloodline-father" value="${
-                        horse.bloodline.father.name
+                        this.horse.bloodline.father.name
                     }"/>
                         <input name="bloodline-father" value="${
-                        horse.bloodline.father.country
+                        this.horse.bloodline.father.country
                     }"/>
                         <label>Rodowód - Matka</label>
                         <input name="bloodline-mother" value="${
-                        horse.bloodline.mother.name
+                        this.horse.bloodline.mother.name
                     }"/>
                         <input name="bloodline-mother" value="${
-                        horse.bloodline.mother.country
+                        this.horse.bloodline.mother.country
                     }"/>
                         <label>Rodowód - Ojciec Matki</label>
                         <input name="bloodline-father-mother" value="${
-                        horse.bloodline.fathermother.name
+                        this.horse.bloodline.fathermother.name
                     }"/>
                         <input name="bloodline-father-mother" value="${
-                        horse.bloodline.fathermother.country
+                        this.horse.bloodline.fathermother.country
                     }"/>
                         <label>Hodowca</label>
                         <input name="breeder-name" value="${
-                        horse.breeder.name
+                        this.horse.breeder.name
                     }"/>
                         <input name="breeder-country" value="${
-                        horse.breeder.country
+                        this.horse.breeder.country
                     }"/>
                         <label>Właściciel</label>
                         <input name="owner-name" value="${
-                        horse.owner.name
+                        this.horse.owner.name
                     }"/>
                         <input name="owner-country" value="${
-                        horse.owner.country
+                        this.horse.owner.country
                     }"/>
                         </div>
                     </div>
